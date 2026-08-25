fb211f41311e513f0e79356a315e5d9b05896322
Kirsten Imam 3 <imam.kj@obmontessori.edu.ph>
2026-08-25 08:45:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from pyscript import display
 
 x = "Year" #string
 y = 2025 #integer
-z = 3.14 
+z = 3.14 #float
+a = True #boolean
+b = ['student1', 'student2', 'student3'] #list
+c = (1,2,3) #tuple
 
 display(type(x), target="div1") #display output in div 1
 display(type(y), target="div1")
 display(type(z), target="div1")
+display(type(a), target="div1")
+display(type(b), target="div1")
