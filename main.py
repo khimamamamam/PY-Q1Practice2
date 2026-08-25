fe8477ad1d7ac497d62e0305518d16a1c4765557
Kirsten Imam 3 <imam.kj@obmontessori.edu.ph>
2026-08-25 08:38:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pyscript import display
 
 x = "Year" #string
 y = 2025 #integer
+z = 3.14 
 
 display(type(x), target="div1") #display output in div 1
 display(type(y), target="div1")
+display(type(z), target="div1")
